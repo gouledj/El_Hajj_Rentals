10443c7b5d78e618491bfe545b5222d80a5e191e
Adam Polywkan <82613399+Apolywkan@users.noreply.github.com>
2022-10-13 22:00:11 -0600

    Implemented database
    Created classes, serializers and views for all tables from ER diagram and linked them to django backend

--- El_Hajj_Rentals/urls.py
+++ El_Hajj_Rentals/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r'rentals', views.RentalView, 'Rental')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('', include('api.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
+    path('api/', include(router.urls)),
 ]
