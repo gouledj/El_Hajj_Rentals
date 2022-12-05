6f9e463ab6a294b8ec59f3b32e415fbfdfe9c753
infantej2 <jethro.ynfante@gmail.com>
2022-12-04 21:47:52 -0700

    Rent: Order Details
    - create a POST for the rental using customer id
    - display the rental details afterwards

--- frontend/src/components/Rent/OrderDetails.js
+++ frontend/src/components/Rent/OrderDetails.js
@@ -1,32 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "@mui/material/Button";
 import { Link, useLocation } from "react-router-dom";
+import { Card, Typography } from "@mui/material";
+import axios from "axios";
+import { CUSTOMER_API_URL } from "../../constants";
 
 const OrderDetails = () => {
 
   const location = useLocation();
-  const { name, branch, from, to, car, card } = location.state;
+  const { name, branch, from, to, car, card, customerID } = location.state;
+  const [customer, setCustomer] = useState(null);
 
-  console.log(branch)
+  console.log("customer id: " + customerID)
   console.log(car)
-  console.log(card)
+
+  useEffect(() => {
+    console.log(customerID)
+    axios.get(CUSTOMER_API_URL + customerID + "/")
+    .then((response) => {
+       setCustomer(response.data)})
+}, []);
 
   return (
-    <div>
-      <div>
+    <div style={{padding:"20px", display:"inline-block"}}>
+    <Card sx={{justifyContent:"center", width:"100%", p:4}}>
+      <Typography variant="h4">
         Order Details
-      </div>
-      <div>
-        <p>Name: {name}</p>
-        <p>Date of rental: {from + " to " + to}</p>
-        <p>Rental Car: {car.manufacturer + " " + car.model}</p>
-        <p>Branch: {branch.street + ", " + branch.city + ", " + branch.province + " " + branch.postalCode}</p>
-        <p>Credit Card: {card}</p>
-        <p>Total Cost: {car.cost}</p>
-      </div>
+      </Typography>
+      {customer && <div style={{paddingTop:"10px"}}>
+        <Typography sx={{mt:2}}>Name: {customer.firstName + " " + customer.lastName}</Typography>
+        <Typography sx={{mt:1}}>Date of rental: {from + " to " + to}</Typography>
+        <Typography sx={{mt:1}}>Rental Car: {car.manufacturer + " " + car.model}</Typography>
+        <Typography sx={{mt:1}}>Branch: {branch.street + ", " + branch.city + ", " + branch.province + " " + branch.postalCode}</Typography>
+        <Typography sx={{mt:4}}>Total Estimated Cost: ${car.cost}</Typography>
+        <Typography sx={{mt:1}}>Credit card receipt will be emailed to the customer.</Typography>
+      </div>}
+    </Card>
+    <div  style={{paddingTop:"20px"}}>
       <Button variant="contained" component={Link} to={"/Rent"}>
-        Return
+        Done
       </Button>
+    </div>
     </div>
   );
 };
